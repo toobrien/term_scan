81c0b8f6ed4a5346a3d56f5badcfb2e38aa589e5
Taylor O'Brien <toobrien@amazon.com>
2021-06-11 19:47:10 -0700

    contract iterator second attempt

--- data/contract_iterator.py
+++ data/contract_iterator.py
@@ -1,6 +1,3 @@
-from data.months import months
-from json import dumps
-
 class contract_iterator:
     MONTH_DOMAIN = {
         "F": 0,
@@ -16,6 +13,7 @@ class contract_iterator:
         "X": 10,
         "Z": 11
     }
+    # year domain = max contracts listed at one time
     YEAR_DOMAIN = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
     SIDE_DOMAIN = ('A', 'B')
 
@@ -28,41 +26,65 @@ class contract_iterator:
     def set_its(self, its): self.its = its
     def get_its(self): return self.its
 
-    def init_leg_it(self, cur, prev):
+    def in_range(self, cur_vals, m_rng, y_rng):
+        return  cur_vals["m"] <= m_rng[1] and \
+                cur_vals["m"] <= self.MONTH_DOMAIN["Z"] and \
+                cur_vals["y"] <= y_rng[1] and \
+                cur_vals["y"] <= self.YEAR_DOMAIN[-1]
+
+    def init_it(self, cur, prev):
         # sets cur's range and current values by 
         # binding the initializer values. e.g.:
         # 
         # prev current month:           1
         # cur month initializer:        ("+2", "V")
         #
-        # results in:
+        # will result in:
         #
         # cur current month:            3
         # cur range:                    (3, 9)
-        m_r = cur["range"]["m"]
-        m_i = cur["init"]["m"]
-        y_r = cur["range"]["y"] 
-        y_i = cur["init"]["y"]
+        cur_vals = cur["current"]
+        m_rng = cur["range"]["m"]
+        m_ini = cur["init"]["m"]
+        y_rng = cur["range"]["y"] 
+        y_ini = cur["init"]["y"]
 
         # bind range
         for i in (0,1):
-            if "+" in m_i[i]:
-                m_r[i] = prev["current"]["m"][0] + int(m_i[i][1:])
+            if "+" in m_ini[i]:
+                m_rng[i] = prev["current"]["m"] + int(m_ini[i][1:])
             else:
-                m_r[i] = self.MONTH_DOMAIN[m_i[i]]
-            if "+" in y_i[i]:
-                y_r[i] = prev["current"]["y"][0] + int(y_i[i][1:])
+                m_rng[i] = self.MONTH_DOMAIN[m_ini[i]]
+            if "+" in y_ini[i]:
+                y_rng[i] = prev["current"]["y"] + int(y_ini[i][1:])
             else:
-                y_r[i] = int(y_i[i])
+                y_rng[i] = int(y_ini[i])
             
-        # initialize current
-        cur["current"]["m"] = cur["range"]["m"][0]
-        cur["current"]["y"] = cur["range"]["y"][0] 
+        # initialize current month, year
+        cur_vals["m"] = m_rng[0]
+        cur_vals["y"] = y_rng[0]
 
+        # validate range and domain
+        return  self.in_range(cur_vals, m_rng, y_rng)
+
+    def increment_it(self, it):        
+        cur = it["current"]
+        rng = it["range"]
+
+        if cur["m"] < rng["m"][1]:
+            cur["m"] += 1
+        elif cur["y"] < rng["y"][1]:
+            cur["m"] = rng["m"][0]
+            cur["y"] += 1
+        else:
+            return False
+
+        return True
 
     def __iter__(self):
         its = [{
-            # null leg, for binding leg 1
+            # null leg, for binding front leg
+            "id": 0,
             "current": { "m": 0, "y": 0 }, 
             "range": None,
             "init": None
@@ -72,53 +94,63 @@ class contract_iterator:
         for leg in legs:
             its.append({
                 "current": { "m": None, "y": None },
-                "range": { "m": (None, None), "y": (None, None) },
-                "init": { "m": (leg[0], leg[1]), "y": (leg[2], leg[3]) },
+                "range": { "m": [ None, None ], "y": [ None, None ] },
+                "init": { "m": [ leg[0], leg[1] ], "y": [ leg[2], leg[3] ] },
             })
 
         # initialize range and current values
         for i in range(1, len(its)):
-            self.init_leg_it(its[i], its[i - 1])
+            if not self.init_it(its[i], its[i - 1]): 
+                return [ "INVALID SPECIFCATION" ]
 
         self.set_its(its)
+
+        return self
 
     def __next__(self):
         its = self.get_its()
         i = len(its) - 1
         j = i
 
-        while (i > 0):
-            it = its[-i]
-            cur = it["current"]
-            rng = it["range"]
-            if cur["m"] < rng["m"][1]:
-                cur["m"] = cur["m"] + 1
-                break
-            elif cur["y"] < rng["y"][1]:
-                cur["y"] = cur["y"] + 1
-                cur["m"] = rng["m"][0]
-                break
-            else:
-                # end of this leg's loop, start on previous leg
-                # re-initialize this one below
-                i -= 1
+        to_inc = i
+        to_init = None
+        refresh = False
 
-        if i == 0:
-            # front leg finished iterating, all done
-            raise StopIteration
-        else:
-            # some legs need to be re-initialized
-            while (i < j):
-                i += 1
-                self.init_leg_it(its[i], its[i - 1])
+        # account for first iteration
+        legs = [ (it["current"]["m"], it["current"]["y"]) for it in its[1:] ]
 
-        return [ (it["current"]["m"], it["current"]["y"]) for it in its[1:] ]
+        while (to_inc or to_init):
+            if to_inc:
+                success = self.increment_it(its[to_inc])
+                if success:
+                    to_init = to_inc + 1
+                    to_inc = None
+                else:
+                    to_inc -= 1
+                    if to_inc == 0:
+                        raise StopIteration
+            elif to_init:
+                if to_init == 1:
+                    raise StopIteration
+                elif to_init > j: 
+                    to_init = None
+                else:
+                    success = self.init_it(its[to_init], its[to_init - 1])
+                    if (success):
+                        to_init += 1
+                    else:
+                        refresh = True
+                        to_inc = to_init - 1
 
+        if refresh:
+            legs = [ (it["current"]["m"], it["current"]["y"]) for it in its[1:] ]
+
+        return legs
 
 if __name__=="__main__":
     legs = [
         [ "Q", "Z", "0", "1", "A" ],
-        [ "+1", "Z", "0", "1", "B" ]
+        [ "+1", "Z", "+0", "+1", "B" ]
     ]
 
     it = contract_iterator(legs)
